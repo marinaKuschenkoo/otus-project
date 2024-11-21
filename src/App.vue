0d4b61c5aa9ac89824ad69a3b3f0808ceeabdff0
marinaKuschenkoo <marinakuschenko1@yandex.ru>
2024-11-22 00:43:21 +0300

    feat: homework_2

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,152 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="wrapper">
+    <div v-if="!products.length" class="loading">Loading&#8230;</div>
+    <ProductCard v-for="(product, index) in products" :product="product" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+<script setup>
+import { onMounted, ref } from "vue";
+import ProductCard from "./components/ProductCard.vue";
+
+const products = ref({});
+onMounted(() => {
+  fetchData();
+});
+const fetchData = async () => {
+  fetch("https://fakestoreapi.com/products")
+    .then((response) => response.json())
+    .then((data) => {
+      products.value = data;
+    });
+};
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.wrapper {
+  display: grid;
+  gap: 30px;
+  grid-template-columns: 1fr 1fr 1fr;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.loading {
+  position: fixed;
+  z-index: 999;
+  height: 2em;
+  width: 2em;
+  overflow: visible;
+  margin: auto;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.loading:before {
+  content: "";
+  display: block;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.3);
+}
+
+.loading:not(:required) {
+  font: 0/0 a;
+  color: transparent;
+  text-shadow: none;
+  background-color: transparent;
+  border: 0;
+}
+
+.loading:not(:required):after {
+  content: "";
+  display: block;
+  font-size: 10px;
+  width: 1em;
+  height: 1em;
+  margin-top: -0.5em;
+  -webkit-animation: spinner 1500ms infinite linear;
+  -moz-animation: spinner 1500ms infinite linear;
+  -ms-animation: spinner 1500ms infinite linear;
+  -o-animation: spinner 1500ms infinite linear;
+  animation: spinner 1500ms infinite linear;
+  border-radius: 0.5em;
+  -webkit-box-shadow: rgba(0, 0, 0, 0.75) 1.5em 0 0 0,
+    rgba(0, 0, 0, 0.75) 1.1em 1.1em 0 0, rgba(0, 0, 0, 0.75) 0 1.5em 0 0,
+    rgba(0, 0, 0, 0.75) -1.1em 1.1em 0 0, rgba(0, 0, 0, 0.5) -1.5em 0 0 0,
+    rgba(0, 0, 0, 0.5) -1.1em -1.1em 0 0, rgba(0, 0, 0, 0.75) 0 -1.5em 0 0,
+    rgba(0, 0, 0, 0.75) 1.1em -1.1em 0 0;
+  box-shadow: rgba(0, 0, 0, 0.75) 1.5em 0 0 0,
+    rgba(0, 0, 0, 0.75) 1.1em 1.1em 0 0, rgba(0, 0, 0, 0.75) 0 1.5em 0 0,
+    rgba(0, 0, 0, 0.75) -1.1em 1.1em 0 0, rgba(0, 0, 0, 0.75) -1.5em 0 0 0,
+    rgba(0, 0, 0, 0.75) -1.1em -1.1em 0 0, rgba(0, 0, 0, 0.75) 0 -1.5em 0 0,
+    rgba(0, 0, 0, 0.75) 1.1em -1.1em 0 0;
+}
+
+@-webkit-keyframes spinner {
+  0% {
+    -webkit-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -ms-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -ms-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@-moz-keyframes spinner {
+  0% {
+    -webkit-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -ms-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -ms-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@-o-keyframes spinner {
+  0% {
+    -webkit-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -ms-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -ms-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@keyframes spinner {
+  0% {
+    -webkit-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -ms-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -ms-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
 }
 </style>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="product">
+    <div
+      class="product__image"
+      :style="{
+        background: `url(${product.image}) no-repeat center / contain`,
+      }"
+    ></div>
+    <div class="product__description">
+      <div class="product__description_text">
+        <span class="product__title">{{ product.title }}</span>
+        <span class="product__info">{{ product.description }}</span>
+      </div>
+      <span class="product__category">{{ product.category }}</span>
+      <div class="product__rating_wrapper">
+        <div class="product__rating">
+          <span
+            class="product__rate"
+            :class="product.rating.rate > 4 ? 'green' : 'red'"
+            >{{ product.rating.rate }}</span
+          >
+          <span class="product__rating_count"
+            >{{ product.rating.count }} Review</span
+          >
+        </div>
+        <p class="product__price">{{ product.price }} $</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  product: {
+    type: Object,
+    default: {},
+  },
+});
+</script>
+
+<style scoped>
+.product {
+  max-width: 440px;
+  height: 600px;
+  width: 100%;
+  background-color: #f6f6f6;
+  border-radius: 30px;
+}
+.product__image {
+  width: 100%;
+  height: 280px;
+  background-color: white !important;
+}
+.product__description {
+  margin: 16px auto;
+  padding: 30px 10px;
+  display: flex;
+  flex-direction: column;
+  row-gap: 8px;
+  justify-content: space-between;
+}
+.product__description_text {
+  display: flex;
+  flex-direction: column;
+  row-gap: 4px;
+}
+.product__title {
+  font-family: DM Sans, sans-serif;
+  font-size: 32px;
+  font-weight: 700;
+  line-height: 41.66px;
+  text-align: left;
+  text-underline-position: from-font;
+  text-decoration-skip-ink: none;
+  -webkit-line-clamp: 1;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-align: justify;
+}
+.product__info {
+  font-family: DM Sans, sans-serif;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 18.23px;
+  letter-spacing: 0.04em;
+  text-underline-position: from-font;
+  text-decoration-skip-ink: none;
+  -webkit-line-clamp: 3;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-align: justify;
+}
+.product__category {
+  font-family: DM Sans, sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 20.83px;
+  letter-spacing: 0.04em;
+  text-align: left;
+  text-underline-position: from-font;
+  text-decoration-skip-ink: none;
+  color: #888888;
+}
+.product__rating_wrapper {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+.product__rating {
+  display: flex;
+  column-gap: 5px;
+}
+.product__price {
+  font-family: DM Sans, sans-serif;
+  font-size: 24px;
+  font-weight: 700;
+  line-height: 31.25px;
+  letter-spacing: 0.04em;
+  text-align: right;
+  text-underline-position: from-font;
+  text-decoration-skip-ink: none;
+}
+.green {
+  color: #3fcf3d;
+}
+.red {
+  color: #e32222;
+}
+</style>
